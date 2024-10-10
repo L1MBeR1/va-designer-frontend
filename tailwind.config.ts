13f1fdc5b581567e6977a0cae177061b13c0966a
L1MBeR1 <fishpaol@gmail.com>
2024-10-10 15:58:42 +0800

    Install dependencies and setting up the app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,17 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+import { nextui } from '@nextui-org/react'
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+module.exports = {
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		extend: {}
+	},
+	darkMode: 'class',
+	plugins: [nextui()]
+}
